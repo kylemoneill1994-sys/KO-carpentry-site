5a102c03e572dcee47443995b8296566b12bb02f
kylemoneill1994-sys <kyle.m.oneill1994@gmail.com>
2026-06-07 19:41:51 -0700

    Update chat.js

--- Netlify/Functions/chat.js
+++ Netlify/Functions/chat.js
@@ -5,7 +5,7 @@ exports.handler = async function(event) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY
+      'Authorization': 'Bearer ' + process.env.openai_api_key
     },
     body: JSON.stringify({
       model: 'gpt-4o-mini',
